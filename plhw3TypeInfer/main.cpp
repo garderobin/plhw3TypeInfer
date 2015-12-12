1544a669248aa549b6e653216df8d00f9b2cce22
garderobin <garderobinshot@hotmail.com>
2015-12-11 21:15:19 -0500

    Initial Commit

---
+++ plhw3TypeInfer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  plhw3TypeInfer
+//
+//  Created by Jasmine Liu on 12/11/15.
+//  Copyright © 2015 Jasmine Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
